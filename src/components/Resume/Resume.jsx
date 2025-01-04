bf63e7895c3c9557a8dcfd118b9a51be2f076d64
Shai <benhemoshai@gmail.com>
2025-01-04 20:22:38 +0200

    Added resume download link

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -69,8 +69,9 @@ export default function Resume() {
 
         <div className="text-center mt-8">
           <a
-            href="#"
+            href="./Shai-Ben-Hemo-CV.pdf"
             className="inline-block bg-blue-600 text-white px-6 py-2 rounded-lg font-medium hover:bg-blue-700 transition-colors"
+            download={true}
           >
             Download Full Resume
           </a>
