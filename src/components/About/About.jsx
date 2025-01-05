f7ac183753d0eef047f7d9430582202f2e7b3023
Shai <benhemoshai@gmail.com>
2025-01-05 14:09:25 +0200

    Modified about section - image position

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <section id="about" className="py-16 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row items-center gap-12 mb-16">
+        <div className="flex flex-col-reverse md:flex-row items-center gap-12 mb-16">
           <AboutImage />
           <AboutContent />
         </div>
